c98864ddfcc8dee87086cf7186df9e374a4b3990
Jan Stolarek <jan.stolarek@ed.ac.uk>
2016-11-02 15:33:13 +0000

    Setup script copies sandbox seetings to submodules

--- sandbox-setup.sh
+++ sandbox-setup.sh
@@ -11,9 +11,21 @@ if [[ -d .cabal-sandbox ]]; then
     done
 fi
 
+# Initialize Cabal sandbox
 cabal sandbox init
+
+# Add modules as source dependencies to the sandbox.
 cabal sandbox add-source modules/algebra-dag
 cabal sandbox add-source modules/algebra-sql
 cabal sandbox add-source modules/dsh
 cabal sandbox add-source modules/dsh-example-queries
 cabal sandbox add-source modules/dsh-sql
+
+# Copy sandbox settings into each submodule directory.  This way it will be
+# possible to run `cabal build` within a subdirectory and re-use the master
+# sandbox for the project.
+cp cabal.sandbox.config modules/algebra-dag
+cp cabal.sandbox.config modules/algebra-sql
+cp cabal.sandbox.config modules/dsh
+cp cabal.sandbox.config modules/dsh-example-queries
+cp cabal.sandbox.config modules/dsh-sql
