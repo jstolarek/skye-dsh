6ff6ea719b011641b02212e5cf2e3240bab77a5c
Jan Stolarek <jan.stolarek@ed.ac.uk>
2016-11-11 10:28:44 +0000

    Sandbox setup now also for DSH submodule

--- sandbox-setup.sh
+++ sandbox-setup.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-if [[ -d .cabal-sandbox ]]; then
-    while true; do
-        read -p "Remove existing cabal sandbox [y/n]? " yn
-        case $yn in
-            [Yy]* ) cabal sandbox delete; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer y or n.";;
-        esac
-    done
-fi
+# Prompts user to remove existing cabal sandbox
+function rmsandbox() {
+    if [[ -d .cabal-sandbox ]]; then
+        while true; do
+            read -p "Remove existing cabal sandbox [y/n]? " yn
+            case $yn in
+                [Yy]* ) cabal sandbox delete; break;;
+                [Nn]* ) return;;
+                * ) echo "Please answer y or n.";;
+            esac
+        done
+    fi
+}
 
-# Initialize Cabal sandbox
+## SETUP MAIN SANDBOX
+
+# Initialize Cabal sandbox and add modules as source dependencies
+rmsandbox
 cabal sandbox init
-
-# Add modules as source dependencies to the sandbox.
 cabal sandbox add-source modules/algebra-dag
 cabal sandbox add-source modules/algebra-sql
 cabal sandbox add-source modules/dsh
 cabal sandbox add-source modules/dsh-example-queries
 cabal sandbox add-source modules/dsh-sql
 
-# Copy sandbox settings into each submodule directory.  This way it will be
-# possible to run `cabal build` within a subdirectory and re-use the master
-# sandbox for the project.
-cp cabal.sandbox.config modules/algebra-dag
-cp cabal.sandbox.config modules/algebra-sql
-cp cabal.sandbox.config modules/dsh
-cp cabal.sandbox.config modules/dsh-example-queries
-cp cabal.sandbox.config modules/dsh-sql
+## SETUP DSH SANDBOX
+
+echo "================== Setting up DSH sandbox ==================="
+cd modules/dsh
+
+# Initialize Cabal sandbox and add modules as source dependencies
+rmsandbox
+cabal sandbox init
+cabal sandbox add-source ../algebra-dag
